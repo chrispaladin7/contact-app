19d2196f1bf7ce09cb6cbf670c307f3cd9ef864e
chrispaladin7 <chrismartey@yahoo.com>
2024-03-03 03:39:40 -0500

    adds contactLists

---
+++ frontend/src/ContactForm.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+const ContactForm = ({ }) => {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+
+    
+}
